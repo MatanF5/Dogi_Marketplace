e8f1eb30d55db0cbe4a7538c0a546eea1476b762
MatanF5 <matanfridlis5@gmail.com>
2022-03-01 17:49:01 +0200

    Fixed wrong input

--- pages/transactions-dashboard.js
+++ pages/transactions-dashboard.js
@@ -54,7 +54,7 @@ export default function transactionsDashBoard() {
   return (
     <div className = "bg-red-700">
       <div className="p-4">
-        <h2 className="text-2xl py-2 text-blue-300">NFT Bought</h2>
+        <h2 className="text-2xl py-2 text-blue-300">NFT Sold</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {NFTS.map((nft, i) => (
             <div key={i} className="border shadow rounded-xl overflow-hidden">
